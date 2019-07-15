f2ead02fd174c3c08d4f267bd7f953409d1b4284
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-07-15 17:02:32 +0200

    Implement cuboid wireframe for selected grid pos

--- machine.rs
+++ machine.rs
@@ -4,6 +4,7 @@ use crate::machine::grid;
 
 use crate::render::{Object, InstanceParams, RenderList};
 
+#[derive(Clone, Debug)]
 pub struct Line {
     pub start: na::Point3<f32>,
     pub end: na::Point3<f32>,
@@ -14,10 +15,15 @@ pub struct Line {
 pub fn render_line(line: &Line, out: &mut RenderList) {
     let center = line.start + (line.end - line.start) / 2.0;
 
+    let d = line.end - line.start;
+
+    // This will roll the line somewhat, works nicely for the cuboid wireframe
+    let up = d.cross(&na::Vector3::x()) + d.cross(&na::Vector3::y()) + d.cross(&na::Vector3::z());
+
     let look_at = na::Isometry3::face_towards(
         &center,
         &line.end,
-        &na::Vector3::z(), // TODO
+        &up,
     );
 
     let scaling = na::Vector3::new(
@@ -38,8 +44,60 @@ pub fn render_line(line: &Line, out: &mut RenderList) {
     );
 }
 
+#[derive(Clone, Debug)]
+pub struct Cuboid {
+    pub center: na::Point3<f32>,
+    pub size: na::Vector3<f32>,
+}
+
+impl Cuboid {
+    pub fn corner_pos(&self, idx: [isize; 3]) -> na::Point3<f32> {
+        let point: na::Point3<f32> = na::convert(na::Point3::from_slice(&idx));
+
+        self.center + 0.5 * point.coords.component_mul(&self.size)
+    }
+}
+
+pub fn render_cuboid_wireframe(
+    cuboid: &Cuboid,
+    thickness: f32,
+    color: &na::Vector4<f32>,
+    out: &mut RenderList,
+) {
+    let lines = vec![
+        // Front
+        ([-1, -1,  1], [ 1, -1,  1]),
+        ([-1,  1,  1], [ 1,  1,  1]),
+        ([-1,  1,  1], [-1, -1,  1]),
+        ([ 1,  1,  1], [ 1, -1,  1]),
+
+        // Back
+        ([-1, -1, -1], [ 1, -1, -1]),
+        ([-1,  1, -1], [ 1,  1, -1]),
+        ([-1,  1, -1], [-1, -1, -1]),
+        ([ 1,  1, -1], [ 1, -1, -1]),
+
+        // Sides
+        ([-1, -1, -1], [-1, -1,  1]),
+        ([ 1, -1, -1], [ 1, -1,  1]),
+        ([-1,  1, -1], [-1,  1,  1]),
+        ([ 1,  1, -1], [ 1,  1,  1]),
+    ];
+
+    for (start_idx, end_idx) in lines {
+        render_line(
+            &Line {
+                start: cuboid.corner_pos(start_idx),
+                end: cuboid.corner_pos(end_idx),
+                thickness,
+                color: *color,
+            },
+            out
+        );
+    }
+}
+
 pub fn render_xy_grid(size: &grid::Vector3, z: f32, out: &mut RenderList) {
-    let thickness = 0.01;
     let color = na::Vector4::new(0.7, 0.7, 0.7, 1.0);
 
     for x in 0 .. size.x + 1 {
