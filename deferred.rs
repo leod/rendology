1446c5525f2556b3d9310f92d97bb7a8e5a9d837
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-07-19 00:34:06 +0200

    Implement deferred shading passes

--- deferred.rs
+++ deferred.rs
@@ -4,6 +4,8 @@ pub use crate::render::shadow::CreationError; // TODO
 
 use log::info;
 
+use nalgebra as na;
+
 use glium::{implement_vertex, uniform, Surface};
 
 use crate::render::{Context, RenderLists, Resources};
@@ -64,15 +66,15 @@ impl DeferredShading {
                 uniform mat4 mat_view;
                 uniform mat4 mat_projection;
 
-                in vec4 position;
-                in vec4 normal;
+                in vec3 position;
+                in vec3 normal;
 
                 smooth out vec4 frag_position;
                 smooth out vec4 frag_normal;
 
                 void main() {
-                    frag_position = mat_model * position;
-                    frag_normal = mat_model * normal;
+                    frag_position = mat_model * vec4(position, 1.0);
+                    frag_normal = vec4(mat3(mat_model) * normal, 1.0);
 
                     gl_Position = mat_projection * mat_view * frag_position;
                 }
@@ -91,8 +93,8 @@ impl DeferredShading {
                 out vec4 f_output3;
 
                 void main() {
-                    f_output1 = vec4(frag_position);
-                    f_output2 = vec4(frag_normal);
+                    f_output1 = frag_position;
+                    f_output2 = frag_normal;
                     f_output3 = color;
                 }
             ",
@@ -126,7 +128,7 @@ impl DeferredShading {
                 uniform sampler2D position_texture;
                 uniform sampler2D normal_texture;
 
-                uniform vec4 light_position;
+                uniform vec3 light_position;
                 uniform vec3 light_attenuation;
                 uniform vec3 light_color;
                 uniform float light_radius;
@@ -139,8 +141,8 @@ impl DeferredShading {
                     vec4 position = texture(position_texture, frag_tex_coord);
                     vec3 normal = normalize(texture(normal_texture, frag_tex_coord).xyz);
 
-                    vec3 light_vector = light_position.xyz - position.xyz;
-                    float light_distance = abs(length(normal.xyz));
+                    vec3 light_vector = light_position - position.xyz;
+                    float light_distance = abs(length(light_vector));
 
                     float diffuse = max(dot(normal, light_vector), 0.0);
 
@@ -205,17 +207,29 @@ impl DeferredShading {
         let quad_vertex_buffer = glium::VertexBuffer::new(
             facade,
             &[
-                QuadVertex { position: [0.0, 0.0, 0.0, 1.0], tex_coord: [0.0, 0.0] },
-                QuadVertex { position: [1.0, 0.0, 0.0, 1.0], tex_coord: [1.0, 0.0] },
-                QuadVertex { position: [1.0, 1.0, 0.0, 1.0], tex_coord: [1.0, 1.0] },
-                QuadVertex { position: [0.0, 1.0, 0.0, 1.0], tex_coord: [0.0, 1.0] },
-            ]
+                QuadVertex {
+                    position: [0.0, 0.0, 0.0, 1.0],
+                    tex_coord: [0.0, 0.0],
+                },
+                QuadVertex {
+                    position: [1.0, 0.0, 0.0, 1.0],
+                    tex_coord: [1.0, 0.0],
+                },
+                QuadVertex {
+                    position: [1.0, 1.0, 0.0, 1.0],
+                    tex_coord: [1.0, 1.0],
+                },
+                QuadVertex {
+                    position: [0.0, 1.0, 0.0, 1.0],
+                    tex_coord: [0.0, 1.0],
+                },
+            ],
         )?;
 
         let quad_index_buffer = glium::IndexBuffer::new(
             facade,
             glium::index::PrimitiveType::TrianglesList,
-            &[0u16, 1, 2, 0, 2, 3]
+            &[0u16, 1, 2, 0, 2, 3],
         )?;
 
         info!("Deferred shading initialized");
@@ -242,6 +256,107 @@ impl DeferredShading {
         render_lists: &RenderLists,
         target: &mut S,
     ) -> Result<(), glium::DrawError> {
+        // Scene pass
+        let output = &[
+            ("f_output1", &self.scene_textures[0]),
+            ("f_output2", &self.scene_textures[1]),
+            ("f_output3", &self.scene_textures[2]),
+        ];
+        let mut framebuffer = glium::framebuffer::MultiOutputFrameBuffer::with_depth_buffer(
+            facade,
+            output.iter().cloned(),
+            &self.depth_texture,
+        )
+        .unwrap(); // TODO: unwrap
+
+        framebuffer.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+        render_lists.solid.render_with_program(
+            resources,
+            context,
+            &Default::default(),
+            &self.scene_program,
+            &mut framebuffer,
+        )?;
+
+        // Lighting pass
+        let draw_params = glium::DrawParameters {
+            //depth_function: glium::DepthFunction::IfLessOrEqual,
+            blend: glium::Blend {
+                color: glium::BlendingFunction::Addition {
+                    source: glium::LinearBlendingFactor::One,
+                    destination: glium::LinearBlendingFactor::One,
+                },
+                alpha: glium::BlendingFunction::Addition {
+                    source: glium::LinearBlendingFactor::One,
+                    destination: glium::LinearBlendingFactor::One,
+                },
+                constant_value: (1.0, 1.0, 1.0, 1.0),
+            },
+            ..Default::default()
+        };
+
+        let mut light_buffer = glium::framebuffer::SimpleFrameBuffer::with_depth_buffer(
+            facade,
+            &self.light_texture,
+            &self.depth_texture,
+        )
+        .unwrap(); // TODO: unwrap
+        light_buffer.clear_color(0.0, 0.0, 0.0, 1.0);
+
+        let mat_scaling = na::Matrix4::new_nonuniform_scaling(&na::Vector3::new(
+            self.window_size.width as f32,
+            self.window_size.height as f32,
+            1.0,
+        ));
+        let mat_orthogonal = na::Matrix4::new_orthographic(
+            0.0,
+            self.window_size.width as f32,
+            0.0,
+            self.window_size.height as f32,
+            -1.0,
+            1.0,
+        ) * mat_scaling;
+        let mat_orthogonal: [[f32; 4]; 4] = mat_orthogonal.into();
+
+        for light in render_lists.lights.iter() {
+            let light_position: [f32; 3] = light.position.coords.into();
+            let light_attenuation: [f32; 3] = light.attenuation.into();
+            let light_color: [f32; 3] = light.color.into();
+
+            let uniforms = uniform! {
+                mat_orthogonal: mat_orthogonal,
+                position_texture: &self.scene_textures[0],
+                normal_texture: &self.scene_textures[1],
+                light_position: light_position,
+                light_attenuation: light_attenuation,
+                light_color: light_color,
+                light_radius: light.radius,
+            };
+
+            light_buffer.draw(
+                &self.quad_vertex_buffer,
+                &self.quad_index_buffer,
+                &self.light_program,
+                &uniforms,
+                &draw_params,
+            )?;
+        }
+
+        // Composition pass
+        let uniforms = uniform! {
+            mat_orthogonal: mat_orthogonal,
+            color_texture: &self.scene_textures[2],
+            lighting_texture: &self.light_texture,
+        };
+
+        target.draw(
+            &self.quad_vertex_buffer,
+            &self.quad_index_buffer,
+            &self.composition_program,
+            &uniforms,
+            &Default::default(),
+        )?;
+
         Ok(())
     }
 
@@ -262,7 +377,7 @@ impl DeferredShading {
 #[derive(Copy, Clone)]
 struct QuadVertex {
     position: [f32; 4],
-    tex_coord: [f32; 2]
+    tex_coord: [f32; 2],
 }
 
 implement_vertex!(QuadVertex, position, tex_coord);
