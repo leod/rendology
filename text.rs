7455f886a2ceb5d053abc513f0f43b5b1344d0ed
leod <subtle.frustration@proton.me>
2019-10-04 22:53:49 +0200

    Show FPS

--- text.rs
+++ text.rs
@@ -45,7 +45,11 @@ impl Font {
     ) {
         let sub_trans = na::Matrix4::new_translation(&na::Vector3::new(0.0, -1.0, 0.0));
         let scale = na::Matrix4::new_scaling(size);
-        let trans = na::Matrix4::new_translation(&na::Vector3::new(pos.x, self.window_size.height as f32 - pos.y, 0.0));
+        let trans = na::Matrix4::new_translation(&na::Vector3::new(
+            pos.x,
+            self.window_size.height as f32 - pos.y,
+            0.0,
+        ));
         let matrix: [[f32; 4]; 4] = (self.projection * trans * scale * sub_trans).into();
 
         let text = glium_text::TextDisplay::new(&self.system, &self.texture, string);
@@ -58,16 +62,14 @@ impl Font {
         );
     }
 
-    fn orthographic_matrix(
-        window_size: glutin::dpi::LogicalSize
-    ) -> na::Matrix4<f32> {
+    fn orthographic_matrix(window_size: glutin::dpi::LogicalSize) -> na::Matrix4<f32> {
         na::Matrix4::new_orthographic(
             0.0,
             window_size.width as f32,
             0.0,
             window_size.height as f32,
             -1.0,
-            0.0
+            0.0,
         )
     }
 }
