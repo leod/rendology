77deb87b1b92b3a6d47ab9f03d0e3975597a2f39
leod <subtle.frustration@proton.me>
2019-10-27 15:13:55 +0100

    Remove dependency on glium_text; will rely on imgui-rs instead

--- text.rs
+++ text.rs
@@ -2,13 +2,15 @@ use nalgebra as na;
 
 use std::{fs, io, path};
 
-use glium_text::{FontTexture, TextSystem};
+use glium::glutin;
+
+//use glium_text::{FontTexture, TextSystem};
 
 pub struct Font {
-    system: TextSystem,
+    /*system: TextSystem,
     texture: FontTexture,
     window_size: glutin::dpi::LogicalSize,
-    projection: na::Matrix4<f32>,
+    projection: na::Matrix4<f32>,*/
 }
 
 impl Font {
@@ -17,22 +19,17 @@ impl Font {
         path: &path::Path,
         window_size: glutin::dpi::LogicalSize,
     ) -> Result<Font, CreationError> {
-        let system = TextSystem::new(facade);
+        /*let system = TextSystem::new(facade);
         let file = fs::File::open(path)?;
         let texture = FontTexture::new(facade, file, 70)?;
-        let projection = Self::orthographic_matrix(window_size);
+        let projection = Self::orthographic_matrix(window_size);*/
 
-        Ok(Font {
-            system,
-            texture,
-            window_size,
-            projection,
-        })
+        Ok(Font {})
     }
 
     pub fn on_window_resize(&mut self, new_window_size: glutin::dpi::LogicalSize) {
-        self.window_size = new_window_size;
-        self.projection = Self::orthographic_matrix(new_window_size);
+        /*self.window_size = new_window_size;
+        self.projection = Self::orthographic_matrix(new_window_size);*/
     }
 
     pub fn draw<S: glium::Surface>(
@@ -43,23 +40,10 @@ impl Font {
         string: &str,
         target: &mut S,
     ) {
-        let sub_trans = na::Matrix4::new_translation(&na::Vector3::new(0.0, -1.0, 0.0));
-        let scale = na::Matrix4::new_scaling(size);
-        let trans = na::Matrix4::new_translation(&na::Vector3::new(pos.x, 1.0 - pos.y, 0.0));
-        let matrix: [[f32; 4]; 4] = (self.projection * trans * scale * sub_trans).into();
-
-        let text = glium_text::TextDisplay::new(&self.system, &self.texture, string);
-        glium_text::draw(
-            &text,
-            &self.system,
-            target,
-            matrix,
-            (color.x, color.y, color.z, color.w),
-        );
     }
 
     fn orthographic_matrix(window_size: glutin::dpi::LogicalSize) -> na::Matrix4<f32> {
-        let aspect_ratio = 1.0; //9.0 / 16.0;
+        /*let aspect_ratio = 1.0; //9.0 / 16.0;
 
         na::Matrix4::new_orthographic(
             0.0,
@@ -70,7 +54,9 @@ impl Font {
             aspect_ratio,
             -1.0,
             0.0,
-        ) * na::Matrix4::new_nonuniform_scaling(&na::Vector3::new(1.0, aspect_ratio, 1.0))
+        ) * na::Matrix4::new_nonuniform_scaling(&na::Vector3::new(1.0, aspect_ratio, 1.0))*/
+
+        na::Matrix4::identity()
     }
 }
 
