680e817be9e8e675995f5c23642bf074af95bf33
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-11-28 21:26:15 +0100

    Collect rendering inputs in render::stage

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 pub mod shader;
 pub mod camera;
 pub mod error;
+pub mod fxaa;
 pub mod instancing;
 pub mod machine;
 pub mod object;
@@ -10,7 +11,7 @@ pub mod render_list;
 pub mod resources;
 pub mod scene;
 pub mod screen_quad;
-pub mod fxaa;
+pub mod stage;
 
 pub use camera::{Camera, EditCameraView};
 pub use error::{CreationError, DrawError};
@@ -20,26 +21,4 @@ pub use pipeline::Pipeline;
 pub use render_list::{Instance, RenderList};
 pub use resources::Resources;
 pub use screen_quad::ScreenQuad;
-
-use nalgebra as na;
-
-#[derive(Debug, Clone)]
-pub struct Context {
-    pub camera: Camera,
-    pub elapsed_time_secs: f32,
-    pub tick_progress: f32,
-    pub main_light_pos: na::Point3<f32>,
-    pub main_light_center: na::Point3<f32>,
-}
-
-impl_uniform_input!(
-    Context,
-    self => {
-        viewport: Vec4 => self.camera.viewport.into(),
-        mat_projection: Mat4 => self.camera.projection.into(),
-        mat_view: Mat4 => self.camera.view.into(),
-        main_light_pos: Vec3 => self.main_light_pos.coords.into(),
-        elapsed_time_secs: Float => self.elapsed_time_secs,
-        tick_progress: Float => self.tick_progress,
-    },
-);
+pub use stage::{Context, Light, RenderLists};
