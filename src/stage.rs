084935d1ce940a154bce1387180a4e4f0d586e96
leod <subtle.frustration@proton.me>
2019-12-04 13:33:49 +0100

    Add working example with rotating cube

--- src/stage.rs
+++ src/stage.rs
@@ -5,8 +5,6 @@ use crate::{scene, Camera, RenderList};
 #[derive(Debug, Clone)]
 pub struct Context {
     pub camera: Camera,
-    pub elapsed_time_secs: f32,
-    pub tick_progress: f32,
     pub main_light_pos: na::Point3<f32>,
     pub main_light_center: na::Point3<f32>,
 }
@@ -18,8 +16,7 @@ impl_uniform_input!(
         mat_projection: Mat4 => self.camera.projection.into(),
         mat_view: Mat4 => self.camera.view.into(),
         main_light_pos: Vec3 => self.main_light_pos.coords.into(),
-        elapsed_time_secs: Float => self.elapsed_time_secs,
-        tick_progress: Float => self.tick_progress,
+        tick_progress: Float => 0.0, // TODO
     },
 );
 
